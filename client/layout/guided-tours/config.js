ffb16b8f7a345f1c604af3060736a13f09401bbf
Marek Hrabe <marekhrabe@me.com>
2016-10-10 17:50:09 +0200

    Initial implementation of Site Title tour

--- client/layout/guided-tours/config.js
+++ client/layout/guided-tours/config.js
@@ -4,8 +4,10 @@
 import { combineTours } from 'layout/guided-tours/config-elements';
 import { MainTour } from 'layout/guided-tours/main-tour';
 import { DesignShowcaseWelcomeTour } from 'layout/guided-tours/design-showcase-welcome-tour';
+import { SiteTitleTour } from 'layout/guided-tours/site-title-tour';
 
 export default combineTours( {
 	main: MainTour,
 	designShowcaseWelcome: DesignShowcaseWelcomeTour,
+	siteTitle: SiteTitleTour,
 } );
